7a047813514e69baab04e7cf9e18967dbecde9c8
sarbazk <me@sarbazk.com>
2020-02-05 16:22:32 +0330

    update jibres class

--- includes/class-jibres.php
+++ includes/class-jibres.php
@@ -13,7 +13,25 @@ class jibres
 		$this->$req($args);
 	}
 
-	private function get_products($args)
+	private function get($data, $post = null)
+	{
+		$ch = curl_init();
+		
+		curl_setopt($ch, CURLOPT_URL, "https://jibres.com/fa/api/v1/{STORE}/products/list");
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		if ($post) {
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		}
+		
+		$get = curl_exec($ch);
+		
+		curl_close($ch);
+
+		return json_encode($get);
+	}
+
+	public function get_products($args)
 	{
 
 		
